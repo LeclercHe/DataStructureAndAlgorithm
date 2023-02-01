d67ca35f76de3fcbda0ad054ec3d5f510bef23a0
LeclercHe <LeclercHe@qq.com>
2023-02-01 17:40:10 +0800

    second commit

--- Stack_BaseLinkList.cpp
+++ Stack_BaseLinkList.cpp
@@ -1,11 +1,57 @@
 #include<stdio.h>
-
-struct Node {
+#include<stdlib.h>
+typedef struct Node {
 	int data;
 	struct Node* next;
-};
+}Node,*PNode;
+PNode top = NULL;
 
+void Push(int x)
+{
+	PNode ptmp = (PNode)malloc(sizeof(PNode));
+	ptmp->data = x;
+	ptmp->next = top;
+	top = ptmp;
+}
+void Pop()
+{
+	PNode ptmp;
+	if (top == NULL) return;
+	ptmp = top;
+	top = top->next;
+	free(ptmp);
+}
+int Top()
+{
+	return top->data;
+}
+bool IsEmpty()
+{
+	if (top == NULL)
+	{
+		printf("stack empty!\n");
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+void Print()
+{
+	PNode ptmp = top;
+	while (ptmp)
+	{
+		printf("%d ", ptmp->data);
+		ptmp = ptmp->next;
+	}
+	printf("\n");
+}
 int main()
 {
+	Push(1); Print();
+	Push(5); Print(); 
+	Push(9); Print();
+	Pop(); Print();
 
 }
